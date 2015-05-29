cde14aa6d67628ac87d2b05978df066614a6c53d
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-05-29 23:00:36 +0200

    Add an abstract template class representing an dictionary

---
+++ src/Dictionary.cpp
@@ -0,0 +1,7 @@
+#include "Dictionary.h"
+
+template <class T>
+Dictionary<T>::Dictionary() {}
+
+template <class T>
+Dictionary<T>::~Dictionary() {}
---
+++ src/Dictionary.h
@@ -0,0 +1,21 @@
+#ifndef Dictionary_H
+#define Dictionary_H
+
+#include <string>
+#include <vector>
+
+template <class T>
+class Dictionary {
+public:
+  Dictionary(void) {};
+  virtual ~Dictionary(void) {};
+  virtual void add_entry(T) = 0;
+  virtual void dictionary_from_file(std::string) = 0;
+  virtual std::vector<T> randomize_vector(int limit = 5) = 0;
+  virtual void print_dictionary(void) = 0;
+  virtual int size() = 0;
+ 
+};
+
+#endif	/* Dictionary_H */
+  
