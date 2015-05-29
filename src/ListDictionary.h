fe0addcb2494609bbf60366a012e2972962815bc
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-05-30 00:00:30 +0200

    Add implementations of some functions for the dictionary

--- src/ListDictionary.h
+++ src/ListDictionary.h
@@ -4,20 +4,46 @@
 #include "Dictionary.h"
 #include <string>
 #include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
 
 template <class T>
 class ListDictionary : public Dictionary<T> {
-
+  
 public:
-  ListDictionary(void);
-  ~ListDictionary(void);
-  void add_entry(T x) override;
-  void dictionary_from_file(std::string) override;
-
+  ListDictionary() {}
+  virtual ~ListDictionary() {}
+  void add_entry(T x) {
+    _dictionary.push_back(x);
+  }
+  void dictionary_from_file(const std::string filename) {
+      std::ifstream file(filename);
+      std::string line;
+      if(!file.is_open()) {
+	perror("Error open ");
+	return;
+      }
+      while(getline(file,line)) {
+	_dictionary.push_back(line);
+      }
+      file.close();      
+  }
+  std::vector<T> randomize_vector(unsigned int limit = 5) {
+    std::vector<T> res;
+    T random_elt;
+    if(limit > 0) {
+      for(unsigned int i = 0; i < limit; i++) {
+	random_elt = _dictionary[rand() % 999];
+	res.push_back(random_elt);
+      }
+    }
+    return res;
+  }
  private:
   std::vector<T> _dictionary;
 
 };
-
 
 #endif	/* ListDictionary_H */
